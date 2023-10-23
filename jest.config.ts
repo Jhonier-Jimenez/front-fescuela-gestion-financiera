3e7f176f1964298840a49d5f442edc87b2063e76
Jhonier Jimenez <jhonier@experimentality.co>
2023-10-23 18:13:10 -0500

    coverage xml adjustments

--- jest.config.ts
+++ jest.config.ts
@@ -32,7 +32,7 @@ const customJestConfig = {
     '!jest.config.ts',
     '!setupTests.ts',
   ],
-  coverageReporters: ['lcov'],
+  coverageReporters: ['cobertura'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
